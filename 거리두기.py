0f7f930e97ca3ae129d5d25600bf9dc14e40368e
nttkor <mpeg4@ymail.com>
2025-08-29 18:48:43 +0900

    거리두기.py

---
+++ 거리두기.py
@@ -0,0 +1,19 @@
+places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], 
+ ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
+ ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], 
+ ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
+ ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]
+ ]
+def solution(places):
+    answer = []
+    mx = 5
+    my = 5
+    dir = [(1,0),(-1,0),(0,1),(0,-1)]
+    being = [ (x,line) for line in places for x in line if places[line][x] == 'P' ]
+    print(being)
+        
+    return answer
+def main():
+    for line in places:
+        print(line)
+    # solution(places)
