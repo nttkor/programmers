a7ed5e45bee4c9f11843d1aef314419f1560f044
nttkor <mpeg4@ymail.com>
2025-08-31 21:21:50 +0900

    이진변환반복

---
+++ 이진변환반복.py
@@ -0,0 +1,25 @@
+def solution(s):
+    answer = [0,0]
+    def bconv(string):
+        b = ''
+        cnt = 0
+        for v in string:
+            if v == '1':
+                b += v
+            else:
+                cnt += 1
+        return bin(len(b)), cnt
+
+    i=0
+    st = s
+    while st != '1':
+        st, cnt = bconv(st)
+        slen = len(st)
+        answer[0] +=1 
+        answer[1] += cnt
+        i += 1
+    return answer
+
+s = "110010101001"
+x = solution(s)
+print(x)
