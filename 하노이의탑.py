878b38e10399538e7ed757b870be663b595c810a
nttkor <mpeg4@ymail.com>
2025-09-01 01:04:52 +0900

    하노이의탑

--- 하노이의탑.py
+++ 하노이의탑.py
@@ -2,7 +2,9 @@ def solution(n):
     def hanoi(n, fr, to, tem):
         if n == 1:
             print(f'{fr} to {to}')
+            return
         hanoi(n-1,fr, tem, to)
+        print(f'{fr} to {to}')
         hanoi(n-1, tem,to,fr)
     hanoi(n,1,3,2)
 solution(2)
