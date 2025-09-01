09bcba75246c29bb63ebb64f5e9daea10cad3a1a
nttkor <mpeg4@ymail.com>
2025-09-01 17:12:33 +0900

    .

--- 하노이의탑블르투포스dfs.py
+++ 하노이의탑블르투포스dfs.py
@@ -1,3 +1,8 @@
+def print_tower(tower):
+    for i, peg in enumerate(tower):
+        print(f"{chr(i+65)}: {peg}")
+    print()
+
 def solution(n):
     initial = [[i for i in range(n, 0, -1)], [], []]
     goal = [[], [i for i in range(n, 0, -1)], []]
@@ -18,9 +23,17 @@ def solution(n):
         visited.add(t_tuple)
 
         if tower == goal:
-            print("Solved in", len(moves), "moves:")
-            for move in moves:
-                print(f"Move disk from {chr(move[0]+65)} to {chr(move[1]+65)}")
+            print("Solved in", len(moves), "moves:\n")
+            print("Initial state:")
+            print_tower(initial)
+
+            current_state = [peg[:] for peg in initial]
+            for idx, move in enumerate(moves, 1):
+                i, j = move
+                disk = current_state[i].pop()
+                current_state[j].append(disk)
+                print(f"Step {idx}: Move disk from {chr(i+65)} to {chr(j+65)}",*current_state)
+                # print_tower(current_state)
             return
 
         for i in range(3):
@@ -30,7 +43,8 @@ def solution(n):
             for j in range(3):
                 if i == j:
                     continue
-                if not tower[j] or tower[j][-1] > disk:
+                if not tower[j] or tower[j][-1] > disk:  #유효성검사
                     new_tower = [peg[:] for peg in tower]
                     new_tower[j].append(new_tower[i].pop())
                     stack.append((new_tower, moves + [(i, j)]))
+solution(3)
